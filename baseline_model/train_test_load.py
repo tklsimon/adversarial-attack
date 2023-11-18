b73404f51cf5f4b49383a62fb070a797766414b3
tklsimon <126179157+tklsimon@users.noreply.github.com>
2023-11-18 15:36:22 +0800

    Update code structure
    Update code structure

--- baseline_model/train_test_load.py
+++ baseline_model/train_test_load.py
@@ -1,32 +1,32 @@
-from argparse import ArgumentParser
-
-from torch.nn import Module
-from torch.utils.data import Dataset
-
-from dataset import dataset
 from model import model_selector
+from dataset import dataset
 from scenario.scenario import Scenario
 from scenario.base_scenario import BaseScenario
 
+from torch.nn import Module
+from torch.utils.data import Dataset
+from argparse import ArgumentParser
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='PyTorch ResNet CIFAR10 Training')
+
     # model parameters
     parser.add_argument('--lr', default=3e-4, type=float, help='learning rate')
     parser.add_argument('--batch_size', default=64, type=int, help='batch size')
     parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
     parser.add_argument('--weight_decay', default=1e-5, type=float, help='weight decay')
+    parser.add_argument('--load_path', default=None, type=str, help='load from checkpoint')
+    parser.add_argument('--save_path', default=None, type=str, help='save checkpoint')
+    parser.add_argument('--layers', default=18, type=int, help='no. of layers in model')
+    parser.add_argument('--clean_model', default=True, action='store_false', help='load online pretrained parameters')
+    parser.add_argument('--model_type', default='', type=str, help='custom or default model')
     # train and test parameters
     parser.add_argument('--train_epochs', default=10, type=int, help='no. of epochs for train')
     parser.add_argument('--train_eval_ratio', default=0.99, type=float, help='ratio for train-eval split')
     parser.add_argument('--test_only', default=False, action='store_true', help='only test model')
     parser.add_argument('--dry_run', default=False, action='store_true', help='will not train or test model')
     parser.add_argument('--load_data', default=False, action='store_true', help='download data if not available')
-    # model parameter
-    parser.add_argument('--load_path', default=None, type=str, help='load from checkpoint')
-    parser.add_argument('--save_path', default=None, type=str, help='save checkpoint')
-    parser.add_argument('--layers', default=18, type=int, help='no. of layers in model')
-    parser.add_argument('--clean_model', default=True, action='store_false', help='load online pretrained parameters')
-    parser.add_argument('--model_type', default='', type=str, help='custom or default model')
+
     args = parser.parse_args()
 
     print("*** train-test-load script ***")
