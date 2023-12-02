b9c81cf1c22432c11e7b6bfef6318079f9e4d9eb
Vini3x3 <vincent.hanghang@gmail.com>
2023-12-02 11:47:52 +0800

    improve visualization script

--- pgd_visualize_script.py
+++ pgd_visualize_script.py
@@ -1,6 +1,7 @@
 import os
 from argparse import ArgumentParser
 
+import torch
 import torchvision.transforms as transforms
 from torch import Tensor
 from torch.nn import Module
@@ -10,12 +11,18 @@ from attack.pgd import PGD
 from dataset import dataset
 from model import model_selector
 
+
+def save_tensor_as_img(tensor: Tensor, file_path: str):
+    transforms.ToPILImage()(tensor).save(file_path)
+
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Visualization Script')
     # model parameter
     parser.add_argument('--layers', default=18, type=int, help='no. of layers in model')
-    parser.add_argument('--pretrain_model', default=True, action='store_false', help='load online pretrained parameters')
-    parser.add_argument('--batch_size', default=4, type=int, help='batch size')
+    parser.add_argument('--pretrain_model', default=True, action='store_false',
+                        help='load online pretrained parameters')
+    parser.add_argument('--batch_size', default=10, type=int, help='batch size')
     # train and test parameters
     parser.add_argument('--load_path', default=None, type=str, help='load from checkpoint')
     parser.add_argument('--load_data', default=False, action='store_true', help='download data if not available')
@@ -42,11 +49,13 @@ if __name__ == '__main__':
     assert args.image_index_start >= 0
     assert args.image_index_end <= len(data_set)
 
-    subset_indices = [i for i in range(args.image_index_start, args.image_index_end + 1)]
+    subset_indices = [i for i in range(args.image_index_start, args.image_index_end)]
 
     subset = Subset(data_set, subset_indices)
 
     dataloader = DataLoader(subset, batch_size=args.batch_size, shuffle=False, num_workers=2)
+
+    attacker: Module = PGD(model, args.epsilon, args.alpha, args.noise_epochs)
 
     augmented_path = os.path.join("img", args.img_path)
     img_dir: str = os.path.dirname(augmented_path)
@@ -54,22 +63,13 @@ if __name__ == '__main__':
         os.makedirs(img_dir)
 
     for (inputs, labels) in dataloader:
-        _input = inputs[1]
-        _label = labels[1]
-
-        pil_image = transforms.ToPILImage()(_input)
-        pil_image.save('img/ori.jpg')
-
-        attacker: Module = PGD(model, args.epsilon, args.alpha, args.noise_epochs)
         blurred_tensor: Tensor = attacker(inputs, labels)
-        _blurred_tensor = blurred_tensor[1]
+        noise_tensor: Tensor = (blurred_tensor - inputs) / args.alpha
+        prediction: Tensor = model(blurred_tensor)
 
-        blurred_image = transforms.ToPILImage()(_blurred_tensor)
-        blurred_image.save('img/blurred.jpg')
-
-        noise_tensor: Tensor = _blurred_tensor - _input
-        noise_tensor /= args.alpha
-        noise_image = transforms.ToPILImage()(noise_tensor)
-        noise_image.save('img/noise.jpg')
-
-        print("target=", dataset.get_cifar10_targets()[_label.item()])
+        for _ in range(inputs.shape[0]):
+            true_label: str = dataset.get_cifar10_targets()[labels[_].item()]
+            pred_label: str = dataset.get_cifar10_targets()[torch.argmax(prediction[_]).item()]
+            save_tensor_as_img(inputs[_], 'img/ori' + str(_) + '_' + true_label + '.jpg')
+            save_tensor_as_img(blurred_tensor[_], 'img/blurred' + str(_) + '_' + pred_label + '.jpg')
+            save_tensor_as_img(noise_tensor[_], 'img/noise' + str(_) + '.jpg')
