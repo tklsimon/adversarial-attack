591acf093610c64181dbd2989549170223e23fc8
Vini3x3 <vincent.hanghang@gmail.com>
2023-12-10 11:41:26 +0800

    add soft label

--- soft_defense_scenario.py
+++ soft_defense_scenario.py
@@ -20,6 +20,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', default=64, type=int, help='batch size')
     parser.add_argument('--momentum', default=0.9, type=float, help='momentum')
     parser.add_argument('--weight_decay', default=1e-5, type=float, help='weight decay')
+    parser.add_argument('--soft_label', default=0.0, type=float, help='label smoothing factor')
     # train and test parameters
     parser.add_argument('--train_epochs', default=10, type=int, help='no. of epochs for train')
     parser.add_argument('--test_val_ratio', default=0.5, type=float, help='ratio for train-eval split')
@@ -76,6 +77,7 @@ if __name__ == '__main__':
                                                      momentum=args.momentum,
                                                      weight_decay=args.weight_decay,
                                                      test_val_ratio=args.test_val_ratio,
+                                                     soft_label=args.soft_label,
                                                      model=model,
                                                      attacker=attacker,
                                                      train_set=train_set,
